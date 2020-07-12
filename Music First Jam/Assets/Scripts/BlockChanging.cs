cea993dd61cc3c33cf16c6773d96f4f0cc715fdc
thenellg <thenellg@oregonstate.edu>
2020-07-12 16:05:37 -0700

    Added Unity project with general gameplay with filler assets

---
+++ Music First Jam/Assets/Scripts/BlockChanging.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockChanging : MonoBehaviour
+{
+    public float volume = 0.2f;
+    public int showingGroup;
+    public GameObject group1;
+    public GameObject group2;
+    public GameObject group3;
+    public GameObject group4;
+    public AudioSource music1;
+    public AudioSource music2;
+    public AudioSource music3;
+    public AudioSource music4;
+    public Sprite color1;
+    public Sprite color2;
+    public Sprite color3;
+    public Sprite color4;
+    public Image currentInstrument;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showingGroup = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (showingGroup == 1)
+        {
+            group1.SetActive(true);
+            group2.SetActive(false);
+            group3.SetActive(false);
+            group4.SetActive(false);
+            musicChange(1);
+            //UIChange(1);
+        }
+        else if (showingGroup == 2)
+        {
+            group1.SetActive(false);
+            group2.SetActive(true);
+            group3.SetActive(false);
+            group4.SetActive(false);
+            musicChange(2);
+            //UIChange(2);
+        }
+        else if (showingGroup == 3)
+        {
+            group1.SetActive(false);
+            group2.SetActive(false);
+            group3.SetActive(true);
+            group4.SetActive(false);
+            musicChange(3);
+            //UIChange(3);
+        }
+        else
+        {
+            group1.SetActive(false);
+            group2.SetActive(false);
+            group3.SetActive(false);
+            group4.SetActive(true);
+            musicChange(4);
+            //UIChange(4);
+        }
+
+    }
+
+    void musicChange(int songPlaying)
+    {
+        music1.volume = 0;
+        music2.volume = 0;
+        music3.volume = 0;
+        music4.volume = 0;
+        if (songPlaying == 1)
+        {
+            music1.volume = volume;
+        }
+        else if (songPlaying == 2)
+        {
+            music2.volume = volume;
+        }
+        else if (songPlaying == 3)
+        {
+            music3.volume = volume;
+        }
+        else if (songPlaying == 4)
+        {
+            music4.volume = volume;
+        }
+    }
+
+    void UIChange(int imageSelect)
+    {
+        if (imageSelect == 1)
+        {
+            currentInstrument.sprite = color1;
+        }
+        else if (imageSelect == 2)
+        {
+            currentInstrument.sprite = color2;
+        }
+        else if (imageSelect == 3)
+        {
+            currentInstrument.sprite = color3;
+        }
+        else if (imageSelect == 4)
+        {
+            currentInstrument.sprite = color4;
+        }
+    }
+}
---
+++ Music First Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //object controller and controller support
+    public GameObject objectController;
+    //ControllerSupport controls;
+
+    //Movement Variables
+    public CharacterController2D controller;
+    float horizontalMovement = 0f;
+    public float moveSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    public void Awake()
+    {
+        /*
+        //Trying the new input system. Doesn't work for continuous movement yet so I'm
+        //scrapping the idea
+        controls = new ControllerSupport();
+
+        //repeat this formula for other controls
+        controls.Gameplay.changeInstrument.performed += ctx => ChangeInstruments();
+        controls.Gameplay.leftMove.performed += ctx => moveLeft();
+        controls.Gameplay.rightMove.performed += ctx => moveRight();
+        */
+    }
+
+    public void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        //Change Instruments
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ChangeInstruments();
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+
+    void ChangeInstruments()
+    {
+        if (objectController.GetComponent<BlockChanging>().showingGroup != 4)
+        {
+            objectController.GetComponent<BlockChanging>().showingGroup++;
+        }
+        else
+        {
+            objectController.GetComponent<BlockChanging>().showingGroup = 1;
+        }
+    }
+
+    /*
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+    */
+}
