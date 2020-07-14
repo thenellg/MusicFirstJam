57d268287b73c5100ddf283496025e553ae5629a
thenellg <thenellg@oregonstate.edu>
2020-07-13 22:30:17 -0700

    Fixed button rotation issue

---
+++ Music First Jam/Assets/Scripts/CounterRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterRotate : MonoBehaviour
+{
+    public GameObject currObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, 0f);
+    }
+}
