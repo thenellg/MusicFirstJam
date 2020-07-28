b69ab306403ca880f3f9d4a447ca66076381b7d9
thenellg <thenellg@oregonstate.edu>
2020-07-27 18:33:30 -0700

    Implemented teleport

---
+++ Music First Jam/Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject teleportSpot;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //some sort of animation to hide?
+
+            collision.transform.position = teleportSpot.transform.position;
+        }
+    }
+}
