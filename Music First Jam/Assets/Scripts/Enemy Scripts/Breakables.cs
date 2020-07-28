65b117b188e1df07e51152f3d620c574b0ed6c28
thenellg <thenellg@oregonstate.edu>
2020-07-27 23:58:00 -0700

    Fixed Tutorial scene post merge

---
+++ Music First Jam/Assets/Scripts/Enemy Scripts/Breakables.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakables : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject spoils;
+    public int damageToPlayer = 0;
+    public bool inArena = false;
+    GameObject arena;
+
+
+    public void Start()
+    {
+        Debug.Log(health);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+        Debug.Log(health);
+    }
+
+    public void Die()
+    {
+        //Add in a death animation?
+        //Instantiate(spoils, transform.position, Quaternion.identity);
+
+        if (inArena)
+            arena.GetComponent<FightArena>().enemiesKilled++;
+
+        Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Attack")
+        {
+            TakeDamage(collision.gameObject.GetComponent<Bullet>().damage);
+        }
+        if (collision.gameObject.tag == "Arena")
+        {
+            arena = collision.gameObject;
+        }
+    }
+}
