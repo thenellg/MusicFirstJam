efc0e8c9fd594ddd135d9c7a0568881f9458b5e4
thenellg <thenellg@oregonstate.edu>
2020-07-30 05:55:20 -0700

    Tweaked levels

---
+++ Music First Jam/Assets/Scripts/LevelHub.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelHub : MonoBehaviour
+{
+    public int doorNum = 0;
+    public TextMeshProUGUI phrase;
+
+    public GameObject HealthUI;
+
+    private void Start()
+    {
+        HealthUI.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (doorNum == 1)
+        {
+            phrase.text = "Graveyard (Tutorial)";
+        }
+        else if (doorNum == 2)
+        {
+            phrase.text = "Dining Room";
+        }
+        else if (doorNum == 3)
+        {
+            phrase.text = "Basement";
+        }
+        else if (doorNum == 4)
+        {
+            phrase.text = "Library";
+        }
+        else if (doorNum == 5)
+        {
+            phrase.text = "Attic";
+        }
+        else
+        {
+            phrase.text = "Pick a level";
+        }
+    }
+}
---
+++ Music First Jam/Assets/Scripts/LevelHubDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHubDoor : MonoBehaviour
+{
+    public int doorNum;
+    public GameObject LevelHubController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LevelHubController.GetComponent<LevelHub>().doorNum = doorNum;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        LevelHubController.GetComponent<LevelHub>().doorNum = 0;
+    }
+
+}
