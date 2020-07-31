d7cec3f2eb09bd53d9844a7b99a6635565a4b1f9
thenellg <thenellg@oregonstate.edu>
2020-07-30 18:51:56 -0700

    Added progression

---
+++ Music First Jam/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,70 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    int levelID;
+
+    public Animator playerAnim;
+    public Animator stringAnim;
+    public Animator curtain;
+
+    public CinemachineVirtualCamera virtualCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelID = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(levelID);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            virtualCam.Follow = transform;
+            playerAnim.SetTrigger("EndLevel");
+            stringAnim.SetTrigger("EndLevel");
+            Invoke("curtainCall", 2f);
+        }
+    }
+
+    void curtainCall()
+    {
+        setPlayerPref();
+        curtain.SetTrigger("curtainDown");
+        Invoke("loading", 2.5f);
+    }
+
+    void loading()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void setPlayerPref()
+    {
+        if (levelID == 2)
+        {
+            PlayerPrefs.SetInt("Graveyard", 1);
+        }
+        else if (levelID == 4)
+        {
+            PlayerPrefs.SetInt("Dining", 1);
+        }
+        else if (levelID == 5)
+        {
+            PlayerPrefs.SetInt("Arcade", 1);
+        }
+        else if (levelID == 6)
+        {
+            PlayerPrefs.SetInt("Library", 1);
+        }
+        else if (levelID == 7)
+        {
+            PlayerPrefs.SetInt("Attic", 1);
+        }
+    }
+}
--- Music First Jam/Assets/Scripts/LevelHub.cs
+++ Music First Jam/Assets/Scripts/LevelHub.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,11 +10,94 @@ public class LevelHub : MonoBehaviour
     public int doorNum = 0;
     public TextMeshProUGUI phrase;
 
+    public GameObject player;
+    public CinemachineVirtualCamera virtualCam;
+
+    public GameObject harp;
+    public Sprite[] harpArt = new Sprite[6];
+    
     public GameObject HealthUI;
+
+    public GameObject[] levelGates = new GameObject[4];
+
+    [SerializeField] int[] levelsBeat;// = new int[5];
+    [SerializeField] int numLevelsBeat = 0;
 
     private void Start()
     {
+        levelsBeat = new int[5];
+
+        numLevelsBeat = 0;
+
+        getPlayerPrefs();
+
+        //PlayerPrefs to get levels beat
+
+        virtualCam.Follow = harp.transform;
+        
+        if (levelsBeat[4] != 1)
+            Invoke("resetCam",4f);
+        
         doorNum = 0;
+
+        setPlayerPrefs();
+        SetHarpImage();
+    }
+
+    void setPlayerPrefs()
+    {
+        PlayerPrefs.SetInt("Graveyard", levelsBeat[0]);
+        PlayerPrefs.SetInt("Dining", levelsBeat[1]);
+        PlayerPrefs.SetInt("Arcade", levelsBeat[2]);
+        PlayerPrefs.SetInt("Library", levelsBeat[3]);
+        PlayerPrefs.SetInt("Attic", levelsBeat[4]);
+    }
+
+    void getPlayerPrefs()
+    {
+        levelsBeat[0] = PlayerPrefs.GetInt("Graveyard", 0);
+        levelsBeat[1] = PlayerPrefs.GetInt("Dining", 0);
+        levelsBeat[2] = PlayerPrefs.GetInt("Arcade", 0);
+        levelsBeat[3] = PlayerPrefs.GetInt("Library", 0);
+        levelsBeat[4] = PlayerPrefs.GetInt("Attic", 0);
+
+    }
+
+    private void SetHarpImage()
+    {
+        if (levelsBeat[0] == 1)
+        {
+            levelGates[0].SetActive(false);
+            numLevelsBeat++;
+        }
+        if (levelsBeat[1] == 1)
+        {
+            levelGates[1].SetActive(false);
+            numLevelsBeat++;
+        }
+        if (levelsBeat[2] == 1)
+        {
+            levelGates[2].SetActive(false);
+            numLevelsBeat++;
+        }
+        if (levelsBeat[3] == 1)
+        {
+            levelGates[3].SetActive(false);
+            numLevelsBeat++;
+        }
+        if (levelsBeat[4] == 1)
+        {
+            numLevelsBeat++;
+        }
+
+        harp.GetComponent<SpriteRenderer>().sprite = harpArt[numLevelsBeat];
+
+        
+    }
+
+    void resetCam()
+    {
+        virtualCam.Follow = player.transform;
     }
 
     private void FixedUpdate()
@@ -40,7 +124,7 @@ public class LevelHub : MonoBehaviour
         }
         else
         {
-            phrase.text = "Pick a level";
+            phrase.text = "Find the missing strings";
         }
     }
 }
